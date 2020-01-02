144b9aee28699eec4cc35752cd5af758d67376cc
scbutton95 <scbutton95@gmail.com>
2020-01-01 22:42:30 -0500

    initial commit

---
+++ Lactic/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Lactic
+//
+//  Created by Sam Button on 12/31/19.
+//  Copyright © 2019 Sam Button. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lactic/ThirdViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ThirdViewController.swift
+//  Lactic
+//
+//  Created by Sam Button on 12/31/19.
+//  Copyright © 2019 Sam Button. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
